f24f6c148ca6b09e9313d9363a69a32a0b3e3217
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2023-11-30 17:12:22 +0600

    created get post delete method for fetching sending data

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ async function run() {
     const postCollection = client.db('communityDB').collection('post');
     const commentCollection = client.db('communityDB').collection('comment');
     const userCollection = client.db('communityDB').collection('users');
+    const newsCollection = client.db('communityDB').collection('news');
+    const tagCollection = client.db('communityDB').collection('tag');
+    const feedbackCollection = client.db('communityDB').collection('feedback');
+
+
 
 
     // jwt
@@ -60,13 +65,13 @@ async function run() {
         return res.status(401).send({ message: 'unauthorized access' });
       }
       const token = req.headers.authorization.split(' ')[1];
-      
+
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
           return res.status(401).send({ message: 'unauthorized access' })
         }
         req.decoded = decoded;
-        console.log("hello",req.decoded);
+        console.log("hello", req.decoded);
         next();
       })
     }
@@ -108,30 +113,43 @@ async function run() {
 
     app.post('/post', async (req, res) => {
       const post = req.body;
-      console.log(post);
+      // console.log(post);
       const result = await postCollection.insertOne(post);
-      console.log(result);
+      // console.log(result);
       res.send(result);
     })
 
 
+    app.delete('/post/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await postCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+
+
+
+
     // Admin
 
-    app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
+    app.get('/users',  async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
 
     })
 
 
-    app.get('/users/admin/:email',verifyToken, async (req, res) => {
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       console.log(email);
       if (!req.decoded || email !== req.decoded.email) {
         console.log(req.decoded);
         return res.status(403).send({ message: 'forbidden access' })
       }
-    
+
       const query = { email: email };
       console.log(query);
       const user = await userCollection.findOne(query);
@@ -141,7 +159,7 @@ async function run() {
       }
       res.send({ admin });
     })
-    
+
 
 
 
@@ -177,12 +195,82 @@ async function run() {
     })
 
 
+    // news
+
+    app.post('/news', async (req, res) => {
+      const news = req.body;
+      console.log(news);
+      const result = await newsCollection.insertOne(news);
+      console.log(result);
+      res.send(result);
+    })
+
+    app.get('/news', async (req, res) => {
+      const result = await newsCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    // tag
+    app.get('/tag', async (req, res) => {
+      const result = await tagCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+
+    app.get('/tag/:tag', async (req, res) => {
+      const tag = req.params.tag;
+
+      // Perform MongoDB search query here
+      const result = await tagCollection.find({ tag: tag }).toArray();
+      res.send(result);
+    });
+
 
 
     // comment
+
+
+    app.get('/comment', async (req, res) => {
+      const result = await commentCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+    app.get('/comment/:title', async (req, res) => {
+      const title = req.params.title;
+      console.log(title);
+
+      // Perform MongoDB search query here
+      const result = await commentCollection.find({ title: title }).toArray();
+      // const result = await commentCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post('/comment', async (req, res) => {
       const user = req.body;
       const result = await commentCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+    // feedback
+
+    app.get('/feedback', async (req, res) => {
+      const result = await feedbackCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+    app.post('/feedback', async (req, res) => {
+      const user = req.body;
+      const result = await feedbackCollection.insertOne(user);
       res.send(result);
     })
 
@@ -193,13 +281,9 @@ async function run() {
 
 
 
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+   
   }
 }
 run().catch(console.dir);
